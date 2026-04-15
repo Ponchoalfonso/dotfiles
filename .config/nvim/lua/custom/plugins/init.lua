a8ba83d65ff38feee4874ab7877d64da60e0ecad
Alfonso Valencia <alfonsitovale@gmail.com>
2026-04-15 15:17:04 -0600

    migrate neovim config to dotfiles repo

---
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,11 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+
+vim.g.netrw_liststyle = 3
+return {
+  require 'custom.plugins.opencode',
+  require 'custom.plugins.vim-tmux-navigator',
+  require 'custom.plugins.netrw',
+}
