bde3e0503b289b7fbed880b32620ff5ca2fb08fa
Alfonso Valencia <alfonsitovale@gmail.com>
2026-09-20 11:48:18 -0600

    dropping pi-sandbox use
    this is kind of YOLO but GH killed my runner action to update the sandbox image with latest pi version
    I will try to figureout a different sandboxing approach in the future

--- scripts/tmux-nvim-pi-sandbox.zsh
+++ scripts/tmux-nvim-pi-sandbox.zsh
@@ -2,11 +2,10 @@
 emulate -L zsh
 setopt errexit nounset pipefail
 
-SCRIPT_DIR="${0:A:h}"
-PI_SANDBOX="$SCRIPT_DIR/pi-sandbox.py"
 NVIM_BIN="${commands[nvim]:-nvim}"
+PI_BIN="${commands[pi]:-pi}"
 NVIM_CMD="${(q)NVIM_BIN}"
-PI_SANDBOX_CMD="${(q)PI_SANDBOX}"
+PI_CMD="${(q)PI_BIN}"
 
 # Replace each command with an interactive shell when it exits so the tmux
 # panes remain usable instead of disappearing.
@@ -18,20 +17,20 @@ if (( $# > 0 )); then
   NVIM_CMD+=" ${(j: :)${(q)argv}}"
 fi
 NVIM_CMD+="$KEEP_OPEN"
-PI_SANDBOX_CMD+="$KEEP_OPEN"
+PI_CMD+="$KEEP_OPEN"
 
-# Create Neovim on the left (75% width) and pi-sandbox on the right (25%).
+# Create Neovim on the left (70% width) and pi on the right (30%).
 # Inside tmux: create a new window in the current session.
 # Outside tmux: create a new tmux session and let tmux choose the session name.
 if [[ -n "${TMUX:-}" ]]; then
   NVIM_PANE="$(tmux new-window -P -F '#{pane_id}' -n editor "$NVIM_CMD")"
-  tmux split-window -h -t "$NVIM_PANE" -l 30% "$PI_SANDBOX_CMD"
+  tmux split-window -h -t "$NVIM_PANE" -l 30% "$PI_CMD"
   tmux select-pane -t "$NVIM_PANE"
 else
   TMUX_CREATED="$(tmux new-session -d -x "$(tput cols)" -y "$(tput lines)" -P -F '#{session_name} #{pane_id}' -n editor "$NVIM_CMD")"
   SESSION_NAME="${TMUX_CREATED%% *}"
   NVIM_PANE="${TMUX_CREATED#* }"
-  tmux split-window -h -t "$NVIM_PANE" -l 30% "$PI_SANDBOX_CMD"
+  tmux split-window -h -t "$NVIM_PANE" -l 30% "$PI_CMD"
   tmux select-pane -t "$NVIM_PANE"
   tmux attach-session -t "$SESSION_NAME"
 fi
